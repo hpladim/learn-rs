9085d7f98e4bf1b3bda372263bdf41d00f9c7975
Hans Petter Ladim <hanspetterl@prediktor.no>
2020-10-10 18:45:38 +0200

    bit on types

---
+++ types/src/main.rs
@@ -0,0 +1,67 @@
+#![allow(unused)]
+fn main() {
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // boolean
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+
+
+    // characters 
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    // tuple
+    let tup = (500, 6.4, 1);
+    // destructuring to variables
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    // more tuple
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+    println!("The value of five_hundred is: {}", y);
+
+    // array
+    let a: [i32; 5] = [1, 2, 3, 4, 5]; //semicolon here
+
+    let first = a[0]; 
+    println!("first {}", first);
+
+    let a = [3; 7]; //equal: assagniment here
+   
+    let second = a[1];
+    println!("second {}", second);
+
+    //Invalid Array Element Access
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+
+    //let element = a[index]; //fails at run time
+
+    //println!("The value of element is: {}", element);
+
+
+
+}
