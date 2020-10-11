a8007563f4b662912c030eee0b769f14e910f5ee
Hans Petter Ladim <hanspetterl@prediktor.no>
2020-10-11 14:12:46 +0200

    Ownership and referencing

---
+++ referencing/src/main.rs
@@ -0,0 +1,30 @@
+
+    fn main() {
+        let mut s = String::from("hello");
+    
+        let r1 = &s; // no problem
+        let r2 = &s; // no problem
+        println!("{} and {}", r1, r2);
+        // r1 and r2 are no longer used after this point
+    
+        let r3 = &mut s; // no problem
+        println!("{}", r3);
+
+        let s = no_dangle();
+        println!("{}", s);
+    }
+
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}
+
+/* 
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+} 
+*/
